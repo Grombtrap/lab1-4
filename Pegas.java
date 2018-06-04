e8f9e4c23b7e1565faac375595469ebcab715a78
Grombtrap <35393916+Grombtrap@users.noreply.github.com>
2018-06-04 09:09:45 +0500

    Create Pegas.java

---
+++ Pegas.java
@@ -0,0 +1,12 @@
+public class Pegas extends Horse{
+
+    public Pegas(String name) {
+        super(name);
+    }
+    public void fly() {
+        System.out.println(name);    
+    }
+    public static void main(String[] args) {
+        
+    }    
+}
